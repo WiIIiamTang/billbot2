901f62ac2d2360bf6d003605977bd1698bb8c34d
William Tang <48343678+WiIIiamTang@users.noreply.github.com>
2023-01-08 03:43:39 -0500

    add coowner

--- notify_starting.py
+++ notify_starting.py
@@ -6,8 +6,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-requests.post(
+r = requests.post(
     os.environ.get("KOI_POSTCHECK_URL"),
     data={"time_started": datetime.now().strftime("%Y-%m-%d %H:%M:%S")},
     headers={"Authorization": "Bearer " + os.environ.get("KOI_AUTH_TOKEN")},
 )
+print(r.status_code, r.content)
